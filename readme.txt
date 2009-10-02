c15930e31b7bb5c0b7edc46b4a5bcecbbc025093
denzel_chia <denzel_chia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-02 04:05:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/vimeo-quicktags/tags/1.2@159857 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== vimeo quicktag ===
+Contributors: denzel chia
+Donate link: http://denzeldesigns.com/2008/12/vimeo-quicktags/
+Tags:vimeo-quicktag, vimeo, quicktag, embed video, embed vimeo video
+Requires at least:2.6
+Tested up to: 2.8.4
+Stable tag:1.2
+
+== Description ==
+
+Enable administrator to embed vimeo video into blog. Options available are the same as provided by vimeo.
+
+== Changelog ==
+
+=1.2=
+
+*Changed plugin path to defined WP_PLUGINS_URL
+*No changes in features.
+
+== Installation ==
+
+
+1. Upload `vimeo quicktag` to the `/wp-content/plugins/` directory and unzip
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Click refresh if vimeo quicktag does not show after activation.
+
+
+== Screenshots ==
+
+1. Vimeo Quicktag dialog window
+
+
---
+++ vimeo.php
@@ -0,0 +1,52 @@
+<?php 
+/*
+Plugin Name: Vimeo Quicktags
+Plugin URI: http://denzeldesigns.com
+Version: 1.2
+Description: insert vimeo quicktag with full embed options as provided by vimeo
+Author: Denzel Chia
+Author URI: http://denzeldesigns.com
+*/
+
+function myplugin_addbuttons() {
+   // Don't bother doing this stuff if the current user lacks permissions
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+     return;
+ 
+   // Add only in Rich Editor mode
+   if ( get_user_option('rich_editing') == 'true') {
+     add_filter("mce_external_plugins", "add_myplugin_tinymce_plugin");
+     add_filter('mce_buttons_3', 'register_myplugin_button');
+   }
+}
+ 
+function register_myplugin_button($buttons) {
+   array_push($buttons, "vimeo");
+   return $buttons;
+}
+
+// determine absolute url path of editor_plugin.js
+function url($type) {
+    //check if defined WordPress Plugins URL
+	if (defined('WP_PLUGINS_URL'))  {
+		
+		return WP_PLUGINS_URL."/". $type ."/editor_plugin.js";
+	
+	}else{
+    //if not assumme it is default location.
+	return "../../../wp-content/plugins/". $type ."/editor_plugin.js";
+	
+	}
+}
+
+// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+function add_myplugin_tinymce_plugin($plugin_array) {
+   $path = url("vimeo-quicktags");
+   $plugin_array['vimeo'] = $path;
+   return $plugin_array;
+}
+ 
+// init process for button control
+add_action('init', 'myplugin_addbuttons');
+
+?>
